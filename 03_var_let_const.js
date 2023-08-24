9e2284dff4990606645cd3a206bb2e7f7145148a
guptasanidhya <guptasanidhya6@gmail.com>
2023-08-24 23:57:34 +0530

    initialize repo

---
+++ 03_var_let_const.js
@@ -0,0 +1,16 @@
+console.log("lets understand what is var const and let")
+// var a = 45;
+// var a ="sandy";
+// console.log(a);
+let b = "Sandy";
+{   
+//you have to initialize b again if you writing b inside block    
+// b="shanu";
+    // console.log(b);
+    let b="Sanidhya";
+    console.log(b);
+}
+
+const f= "SG";
+// f="fg";
+console.log(f);
