56fd802d804a72c93f7a5c689044a4eee642106f
guptasanidhya <guptasanidhya6@gmail.com>
2023-09-26 02:23:02 +0530

    practice

---
+++ 16_Arrays.js
@@ -0,0 +1,4 @@
+let marks_class_12=[91,82,83,84,85,false,"Not Present"];
+console.log(marks_class_12[8]);
+console.log("the length of marks_class_12 is", marks_class_12);
+console.log(typeof( marks_class_12))
---
+++ 17_Array_methods1.js
@@ -0,0 +1,15 @@
+let num=[1,2,3,4];
+let b=num.toString();
+console.log(b,typeof b);
+// let c=num.join(" and ");
+// console.log(c,typeof c);
+// // let d = num.concat(1)
+// let d = num.concat(1,2,3,4)
+// console.log(d,typeof d);
+// let r=num.pop();//pop th last element
+// console.log(num,r,typeof r);
+// let r = num.push('hello');//push a new element
+// console.log(num,r,typeof r);
+
+let r = num.unshift(78);
+console.log(num,r,typeof r);
